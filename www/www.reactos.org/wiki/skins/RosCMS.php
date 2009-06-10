33b92982e3f7cab49c2adca13b20bbcd6e10eeef
Colin Finck <colin@reactos.org>
2009-06-10 22:45:54 +0000

    Add the Google Analytics tracking code
    svn path=/branches/danny-web/; revision=431

--- www/www.reactos.org/wiki/skins/RosCMS.php
+++ www/www.reactos.org/wiki/skins/RosCMS.php
@@ -254,6 +254,18 @@ class RosCMSTemplate extends QuickTemplate {
 
 -->
 <?php endif; ?>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try{
+var pageTracker = _gat._getTracker("UA-610991-1");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
+
 </body></html>
 <?php
 	wfRestoreWarnings();
