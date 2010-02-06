d51e8b4ec86dde63db9a826f2fb4c321903a90b4
Christoph von Wittich <christoph_vw@reactos.org>
2010-02-06 14:59:00 +0000

    add pages for errors 500,502,503 and 504
    svn path=/trunk/; revision=572

---
+++ www/www.reactos.org/errorpages/500.php
@@ -0,0 +1,33 @@
+<?php
+
+require "common.php";
+
+echo "
+<html>
+
+<body bgcolor=\"#000099\">
+<font color=\"white\" face=\"Lucida Console\" size=\"2\">
+
+A problem has been detected and RosCMS has been shut down to prevent damage to your computer.<p>
+
+KERNEL_MODE_EXCEPTION_NOT_HANDLED<p>
+
+If this is the first time you have seen this error screen,<br>
+restart your browser. If this screen appears again, follow<br>
+these steps:<p>
+
+Check to make sure the page you requested is spelled properly.<br> If this is the case press F5 for advanced reload options.<p>
+
+If problems continue, drop an email to ros-web@reactos.org<p>
+
+Technical information:<p>
+
+*** STOP: 0x00000500 ($r_ip, $r_port, $s_ip, $s_port)<p></br>
+
+***&nbsp;&nbsp;&nbsp;$page - Address $server$ref, DateStamp $datestamp
+
+</font>
+</body>
+
+</html>"
+?>
