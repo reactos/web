90210a39e0e2282a020d61de480f31d7386aa04a
Colin Finck <colin@reactos.org>
2010-05-02 14:02:15 +0000

    Add a sample "bugzilla-config.php" to simplify local testing.
    svn path=/trunk/; revision=594

---
+++ www/www.reactos.org_config/bugzilla-config.php
@@ -0,0 +1,16 @@
+<?php
+# The DNS name of the host that the database server runs on.
+$dbbug_host = 'localhost';
+
+# The name of the database
+$dbbug_name = 'bugs';
+
+# Who we connect to the database as.
+$dbbug_user = 'root';
+
+# Enter your database password here. It's normally advisable to specify
+# a password for your bugzilla database user.
+# If you use apostrophe (') or a backslash (\) in your password, you'll
+# need to escape it by preceding it with a '\' character. (\') or (\)
+# (Far simpler just not to use those characters.)
+$dbbug_pass = '';
