e8cb38fcc44e9b99b56f4977f0648456d1f84d19
Marius Przybylski <pay7n@o2.pl>
2011-12-25 21:52:14 +0000

    Remove the site name and site slogan theme features as they are not used. Comment out the code used to display them.
    svn path=/branches/WebsiteRevamp/; revision=671

--- www/www.reactos.org/drupal/sites/all/themes/zen_reactos/templates/page.tpl.php
+++ www/www.reactos.org/drupal/sites/all/themes/zen_reactos/templates/page.tpl.php
@@ -77,14 +77,14 @@
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
     <?php endif; ?>
 
-    <?php if ($site_name || $site_slogan): ?>
+    <?php /* if ($site_name || $site_slogan): ?>
       <div id="name-and-slogan">
         <?php if ($site_name): ?>
           <?php if ($title): ?>
             <div id="site-name"><strong>
               <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
             </strong></div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
+          <?php else: / Use h1 when the content title is empty / ?>
             <h1 id="site-name">
               <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
             </h1>
@@ -95,7 +95,7 @@
           <div id="site-slogan"><?php print $site_slogan; ?></div>
         <?php endif; ?>
       </div><!-- /#name-and-slogan -->
-    <?php endif; ?>
+    <?php endif;*/ ?>
 
     <?php print render($page['header']); ?>
 
