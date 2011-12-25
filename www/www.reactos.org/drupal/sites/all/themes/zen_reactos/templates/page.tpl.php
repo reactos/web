0a9b3990a12b1e43bd09ff709bcb5b2c1a3ad5b7
Marius Przybylski <pay7n@o2.pl>
2011-12-25 21:56:24 +0000

    Display logo unconditionally. Remove the logo theme feature.
    svn path=/branches/WebsiteRevamp/; revision=672

--- www/www.reactos.org/drupal/sites/all/themes/zen_reactos/templates/page.tpl.php
+++ www/www.reactos.org/drupal/sites/all/themes/zen_reactos/templates/page.tpl.php
@@ -73,9 +73,7 @@
 
   <div id="header"><div class="section clearfix">
 
-    <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
 
     <?php /* if ($site_name || $site_slogan): ?>
       <div id="name-and-slogan">
