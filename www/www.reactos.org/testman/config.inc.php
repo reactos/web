b4dd21495da9a6307cedd8f4385f3820188120d4
Pierre Schweitzer <pierre@reactos.org>
2012-02-25 09:22:55 +0000

    [TESTMAN] Switch default source to CMake
    svn path=/trunk/; revision=718

--- www/www.reactos.org/testman/config.inc.php
+++ www/www.reactos.org/testman/config.inc.php
@@ -10,9 +10,9 @@
 	define("ROSCMS_PATH", ROOT_PATH . "roscms/");
 	define("SHARED_PATH", ROOT_PATH . "shared/");
 	define("TESTMAN_PATH", "");
-	
+
 	define("DEFAULT_SEARCH_LIMIT", 10);
-	define("DEFAULT_SEARCH_SOURCE", "Trunk_x86_GCCLin (KVM)");
+	define("DEFAULT_SEARCH_SOURCE", "CMake_x86_GCCLin (KVM)");
 	define("MAX_COMPARE_RESULTS", 5);
 	define("RESULTS_PER_PAGE", 100);
 
