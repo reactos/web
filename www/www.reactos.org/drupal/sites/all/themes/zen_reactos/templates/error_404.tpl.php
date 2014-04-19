ab251a568aeb0cf1c63daad9db0164a3e1d9cc9d
Pierre Schweitzer <pierre@reactos.org>
2014-04-19 11:10:05 +0000

    [DRUPAL] Fix Drupal 404 page.
    Patch by Fabian L.
    
    svn path=/trunk/; revision=858

--- www/www.reactos.org/drupal/sites/all/themes/zen_reactos/templates/error_404.tpl.php
+++ www/www.reactos.org/drupal/sites/all/themes/zen_reactos/templates/error_404.tpl.php
@@ -1,7 +1,7 @@
 <?php
 	header("HTTP/1.0 404 Not Found");
 	
-	function ip_address_to_number($address) 
+	function ip_to_number($address) 
 	{
 		$ips = explode('.', $address);
 		return ($ips[3] + $ips[2] * 256 + $ips[1] * 256 * 256 + $ips[0] * 256 * 256 * 256);
@@ -9,7 +9,7 @@
 	
 	$url = 'http://' . $_SERVER["SERVER_NAME"];
 	$address = $_SERVER["SERVER_NAME"];
-	if(isset($_SERVER["HTTP_REFERER"]))
+	if(isset($_SERVER["HTTP_REFERER"]) and !empty($_SERVER["HTTP_REFERER"]))
   		$address .= ' base at ' . $_SERVER["HTTP_REFERER"];
 ?>
 <!DOCTYPE html>
@@ -35,7 +35,7 @@
 <div id="bsod">
 	<p>A problem has been detected and the ReactOS website has been shut down to prevent damage to your computer.</p>
 
-	<p><?php echo $code; ?></p>
+	<p>404 - Not Found</p>
 
 	<p>If this is the first time you have seen this error screen,<br />
 	restart your browser. If this screen appears again, follow<br />
@@ -48,10 +48,10 @@
 
 	<p>Technical information:</p>
 
-	<p><?php printf('*** STOP: 0x00000%u (0x%08x, 0x%08x, 0x%08x, 0x%08x)', $http_error, ip_address_to_number($_SERVER["REMOTE_ADDR"]), $_SERVER["REMOTE_PORT"], ip_address_to_number($_SERVER["SERVER_ADDR"]), $_SERVER["SERVER_PORT"]); ?></p>
+	<p><?php printf('*** STOP: 0x00000%u (0x%08x, 0x%08x, 0x%08x, 0x%08x)', $http_error, ip_to_number($_SERVER["REMOTE_ADDR"]), $_SERVER["REMOTE_PORT"], ip_to_number($_SERVER["SERVER_ADDR"]), $_SERVER["SERVER_PORT"]); ?></p>
 
 
-	<p><br />***&nbsp;&nbsp;&nbsp;<?php printf('%s - Address %s, DateStamp %08x', $_SERVER["REQUEST_URI"], $address, date("U")); ?></p>
+	<p><br />***&nbsp;&nbsp;&nbsp;<?php printf('Address %s at %s, DateStamp %08x', $_SERVER["REQUEST_URI"], $address, date("U")); ?></p>
 	
 	<p><a href="<?php echo $url; ?>">&raquo; back to our homepage</a></p>
 </div>
