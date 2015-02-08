75b2f9f6fdb406df901e4173d7b148735a56d12e
Kamil Hornicek <kamil.hornicek@reactos.org>
2015-02-08 14:09:46 +0000

    [TESTMAN] - try to fix the total testing time retrieval
    svn path=/trunk/; revision=877

--- www/www.reactos.org/drupal/sites/all/modules/reactos/testman/webservice/buildbot_aggregator.php
+++ www/www.reactos.org/drupal/sites/all/modules/reactos/testman/webservice/buildbot_aggregator.php
@@ -202,6 +202,8 @@
 
     curl_setopt($ch, CURLOPT_FILE, $fp);
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
     $result = curl_exec($ch);
     curl_close($ch);
