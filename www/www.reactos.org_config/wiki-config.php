6fdb6886fc34907c03445860190b1644d8a56591
Colin Finck <colin@reactos.org>
2015-04-16 21:10:38 +0000

    [WIKI] Adapt LocalSettings.php to MediaWiki 1.24.3 and reduce diffs to a LocalSettings.php regenerated from scratch by this MediaWiki version. Move the confidential $wgSecretKey and $wgUpgradeKey to the hidden wiki-config.php as well. They have already been updated on the server for maintaining security.
    svn path=/trunk/; revision=893

--- www/www.reactos.org_config/wiki-config.php
+++ www/www.reactos.org_config/wiki-config.php
@@ -1,6 +1,15 @@
 <?php
 
-$wgDBserver         = "localhost";
-$wgDBname           = "wiki";
-$wgDBuser           = "wiki";
-$wgDBpassword       = "";
+## Database settings
+$wgDBtype = "mysql";
+$wgDBserver = "localhost";
+$wgDBname = "wiki";
+$wgDBuser = "wiki";
+$wgDBpassword = "wiki";
+
+
+$wgSecretKey = "190071721adb67145f9a02d61f4a84cd2a15980845d562ec64750e83f634c92";
+
+# Site upgrade key. Must be set to a string (default provided) to turn on the
+# web installer while LocalSettings.php is in place
+$wgUpgradeKey = "132dccc4e3e9ddf";
