9caad070f4de2f3aa25ac6dd3a3a1a6381fb517a
Pierre Schweitzer <pierre@reactos.org>
2015-11-14 10:47:56 +0000

    [TESTMAN] Whitespaces
    svn path=/trunk/; revision=910

--- www/www.reactos.org/testman/webservice/buildbot_aggregator.php
+++ www/www.reactos.org/testman/webservice/buildbot_aggregator.php
@@ -5,12 +5,12 @@
   PURPOSE:    Aggregator for the Debug Log of ReactOS BuildBot Buildslaves
   COPYRIGHT:  Copyright 2009-2015 Colin Finck <colin@reactos.org>
 */
-	
+
 	require_once("config.inc.php");
 	require_once(TESTMAN_PATH . "connect.db.php");
 	require_once("utils.inc.php");
 	require_once("autoload.inc.php");
-	
+
 	$perf = array(
 		"boot_cycles" => 0,
 		"context_switches" => 0,
@@ -19,10 +19,10 @@
 		"system_calls" => 0,
 		"time" => 0
 	);
-	
+
 	if(!isset($_GET["sourceid"]) || !isset($_GET["password"]) || !isset($_GET["builder"]) || !is_numeric($_GET["platform"]) || !is_numeric($_GET["build"]))
 		die("Necessary information not specified!");
-	
+
 	try
 	{
 		$dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_TESTMAN, DB_USER, DB_PASS);
@@ -32,39 +32,39 @@
 		// Give no exact error message here, so no server internals are exposed
 		die("Could not establish the DB connection");
 	}
-	
+
 	VerifyLogin($_GET["sourceid"], $_GET["password"]);
-	
+
 	// Make sure nobody runs this script multiple times for the same build
 	$stmt = $dbh->prepare("SELECT COUNT(*) FROM winetest_runs WHERE comment = :comment AND source_id = :sourceid");
 	$stmt->bindValue(":comment", "Build " . $_GET["build"]);
 	$stmt->bindParam(":sourceid", $_GET["sourceid"]);
 	$stmt->execute() or die("SQL failed #1");
-	
+
 	if($stmt->fetchColumn())
 		die("The script has already processed this build before!");
-	
+
 	// Read the Buildslave test log
 	$fp = @fopen(BUILDER_URL . rawurlencode($_GET["builder"]) . "/builds/" . $_GET["build"] . "/steps/test/logs/stdio/text", "r");
-	
+
 	if(!$fp)
 		die("Could not open the test log!");
-	
+
 	// Get the revision
 	do
 	{
 		$line = fgets($fp);
 	}
 	while (!preg_match("#.+ ReactOS .+ \(Build [0-9]+-r([0-9]+)\)#", $line, $matches) && !feof($fp));
-	
+
 	$revision = $matches[1];
-	
+
 	if (!$revision)
 		die("Got no revision");
-	
+
 	// Create a WineTest object for accessing the database
 	$t = new WineTest();
-	
+
 	// Find the first 3rd stage boot
 	while (substr($line, 0, 24) != "[SYSREG] Running stage 3" && !feof($fp))
 	{
@@ -73,31 +73,31 @@
 
 		$line = fgets($fp);
 	}
-	
+
 	// Find the boot performance info
 	while (substr($line, 0, 10) != "Boot took " && !feof($fp))
 	{
-		$line = fgets($fp);  
+		$line = fgets($fp);
 	}
-	
+
 	if (!feof($fp))
 	{
 		// We found the boot info
 	   	$cycles = explode(" ", $line, 4);
-	   
+
 		if (is_numeric($cycles[2]))
 			$perf["boot_cycles"] = $cycles[2];
-	      
+
 		// We're in the 3rd stage, increase the reboot count
 		$perf["reboots"]++;
 	}
-	
+
 	// Find the rest of the boot performance info
 	while (substr($line, 0, 12) != "Interrupts: " && !feof($fp))
 	{
-		$line = fgets($fp);  
+		$line = fgets($fp);
 	}
-	
+
 	if(!feof($fp))
 	{
 		// We found the rest of the perf info
@@ -105,18 +105,18 @@
 
 		if (is_numeric($performance[1]))
 			$perf["interrupts"] = $performance[1];
-	   
+
 		if (is_numeric($performance[4]))
-			$perf["system_calls"] = $performance[4];	
-       
+			$perf["system_calls"] = $performance[4];
+
 		if (is_numeric($performance[7]))
 			$perf["context_switches"] = $performance[7];
 	}
-	
+
 	// Get the log for each test
 	$line = "";
 	$test_id = 0;
-	
+
 	while(!feof($fp))
 	{
 		// Find the line with the test information
@@ -128,36 +128,36 @@
 			if (substr($line, 0, 22) == "[SYSREG] Running stage")
 				$perf["reboots"]++;
 		}
-		
+
 		// We might reach end of file here, we're done in this case
 		if(feof($fp))
 			break;
-		
+
 		// Parse the test line
 		if(!preg_match("#Running Wine Test, Module: (.+), Test: ([\S]+)#", $line, $matches))
 			die("Wine Test line is invalid!");
-		
+
 		// Get a Suite ID for this combination
 		$suite_id = $t->getSuiteId($matches[1], $matches[2]);
-		
+
 		// If an error occured, $suite_id will contain the error message
 		if(!is_numeric($suite_id))
 			die($suite_id);
-		
+
 		// Now get the real log
 		$log = "";
-		
+
 		for(;;)
 		{
 			$line = fgets($fp);
-			
+
 			// If this test misses the summary line for some reason, check whether we reached the next test already.
 			// If so, already break the loop here, so that this line won't be on the log for this test.
 			if(substr($line, 0, 27) == "Running Wine Test, Module: ")
 				break;
-			
+
 			$log .= $line;
-			
+
 			// We can easily exceed PHP's memory limit here in case we're reading a bloated log
 			// Stop in this case
 			if(memory_get_usage() > MAX_MEMORY)
@@ -165,32 +165,32 @@
 				$log .= "[TESTMAN] Maximum memory for log exceeded, aborting!";
 				break;
 			}
-			
+
 			// Sysreg might also have noticed a system crash or we even reached the end of the log. Break then.
 			if(substr($line, 0, 9) == "[SYSREG] " || feof($fp))
 				break;
 		}
-		
+
 		// Did we already get a Test ID for this run?
 		if(!$test_id)
 		{
 			$test_id = $t->getTestId($_GET["sourceid"], $revision, "reactos." . $_GET["platform"], "Build " . $_GET["build"]);
-			
+
 			// If an error occured, $test_id will contain the error message
 			if(!is_numeric($test_id))
 				die($test_id);
 		}
-		
+
 		// Finally submit the log
 		$return = $t->submit($_GET["sourceid"], $test_id, $suite_id, $log);
-		
+
 		// If an error occured, $return will contain the error message
 		if($return != "OK")
 			die("submit($test_id, $suite_id, ...) - $return");
 	}
 
 	fclose($fp);
-	
+
 	// The last thing to do is to get the total time the testing took
 	$fp = tmpfile();
 
@@ -221,7 +221,7 @@
 	}
 
 	fclose($fp);
-	
+
 	// If we have a Test ID, finish this test run and terminate with the return message from that function
 	// Otherwise we couldn't find any test information in this log
 	if ($test_id)
--- www/www.reactos.org/testman/webservice/lib/WineTest.class.php
+++ www/www.reactos.org/testman/webservice/lib/WineTest.class.php
@@ -11,10 +11,10 @@
 		public function getTestId($source_id, $revision, $platform, $comment)
 		{
 			global $dbh;
-			
+
 			if(!isset($revision) || !isset($platform))
 				return "Necessary sub-information not specified!";
-			
+
 			// Add a new Test ID with the given information
 			$stmt = $dbh->prepare("INSERT INTO winetest_runs (source_id, revision, platform, comment) VALUES (:sourceid, :revision, :platform, :comment)");
 			$stmt->bindParam(":sourceid", $source_id);
@@ -22,70 +22,70 @@
 			$stmt->bindParam(":platform", $platform);
 			$stmt->bindParam(":comment", $comment);
 			$stmt->execute() or die("GetTestID(): SQL failed #1");
-			
+
 			return $dbh->lastInsertId();
 		}
-		
+
 		public function getSuiteId($module, $test)
 		{
 			global $dbh;
-			
+
 			if(!isset($module) || !isset($test))
 				return "Necessary sub-information not specified!";
-			
+
 			// Determine whether we already have a suite ID for this combination
 			$stmt = $dbh->prepare("SELECT id FROM winetest_suites WHERE module = :module AND test = :test");
 			$stmt->bindParam(":module", $module);
 			$stmt->bindParam(":test", $test);
 			$stmt->execute() or die("GetSuiteID(): SQL failed #1");
 			$id = $stmt->fetchColumn();
-			
+
 			if($id)
 				return $id;
-			
+
 			// Add this combination to the table and return the ID for it
 			$stmt = $dbh->prepare("INSERT INTO winetest_suites (module, test) VALUES (:module, :test)");
 			$stmt->bindParam(":module", $module);
 			$stmt->bindParam(":test", $test);
 			$stmt->execute() or die("GetSuiteID(): SQL failed #2");
-			
+
 			return $dbh->lastInsertId();
 		}
-		
+
 		public function submit($source_id, $test_id, $suite_id, $log)
 		{
 			global $dbh;
-			
+
 			if(!isset($test_id) || !isset($suite_id) || !isset($log))
 				return "Necessary sub-information not specified!";
-			
+
 			// Make sure that we may add information to the test with this Test ID
 			$stmt = $dbh->prepare("SELECT COUNT(*) FROM winetest_runs WHERE id = :testid AND finished = 0 AND source_id = :sourceid");
 			$stmt->bindParam(":testid", $test_id);
 			$stmt->bindParam(":sourceid", $source_id);
 			$stmt->execute() or die("Submit(): SQL failed #1");
-			
+
 			if(!$stmt->fetchColumn())
 				return "No such test or no permissions!";
-			
+
 			// Make sure that this test run does not yet have a result for this test suite
 			$stmt = $dbh->prepare("SELECT COUNT(*) FROM winetest_results WHERE test_id = :testid AND suite_id = :suiteid");
 			$stmt->bindParam(":testid", $test_id);
 			$stmt->bindParam(":suiteid", $suite_id);
 			$stmt->execute() or die("Submit(): SQL failed #2");
-			
+
 			if($stmt->fetchColumn())
 				return "We already have a result for this test suite in this test run!";
-			
+
 			// Get the test name
 			$stmt = $dbh->prepare("SELECT test FROM winetest_suites WHERE id = :id");
 			$stmt->bindParam(":id", $suite_id);
 			$stmt->execute() or die("Submit(): SQL failed #3");
 			$test = $stmt->fetchColumn();
-			
+
 			// Get all summary lines belonging to this test in the whole log (a test may have multiple summary lines)
 			$result = preg_match_all("#^{$test}: ([0-9]+) tests executed \(([0-9])+ marked as todo, ([0-9]+) failure[s]?\), ([0-9]+) skipped.#m", $log, $matches, PREG_PATTERN_ORDER);
-			
+
 			if($result === FALSE)
 			{
 				return "preg_match_all failed!";
@@ -114,7 +114,7 @@
 				$failures = array_sum($matches[3]);
 				$skipped = array_sum($matches[4]);
 			}
-			
+
 			// Get the execution time
 			$result = preg_match_all("#^Test {$test} completed in ([0-9]+\.[0-9]+) seconds.#m", $log, $matches, PREG_PATTERN_ORDER);
 
@@ -142,33 +142,33 @@
 			$stmt->bindParam(":todo", $todo);
 			$stmt->bindParam(":time", $time);
 			$stmt->execute() or die("Submit(): SQL failed #4");
-			
+
 			$stmt = $dbh->prepare("INSERT INTO winetest_logs (id, log) VALUES (:id, COMPRESS(:log))");
 			$stmt->bindValue(":id", (int)$dbh->lastInsertId());
 			$stmt->bindParam(":log", $log);
 			$stmt->execute() or die("Submit(): SQL failed #5");
-			
+
 			return "OK";
 		}
-		
+
 		public function finish($source_id, $test_id, $performance)
 		{
 			global $dbh;
-			
+
 			if(!isset($test_id))
 				return "Necessary sub-information not specified!";
-			
+
 			// Sum up all results and mark this test as finished, so no more results can be submitted for it
 			$stmt = $dbh->prepare(
-				"UPDATE winetest_runs 
-				 SET 
-					finished = 1, 
-					count    = (SELECT SUM(count) FROM  winetest_results WHERE test_id = :testid), 
-					failures = (SELECT SUM(failures) FROM winetest_results WHERE test_id = :testid), 
-					boot_cycles = :boot_cycles, 
+				"UPDATE winetest_runs
+				 SET
+					finished = 1,
+					count    = (SELECT SUM(count) FROM  winetest_results WHERE test_id = :testid),
+					failures = (SELECT SUM(failures) FROM winetest_results WHERE test_id = :testid),
+					boot_cycles = :boot_cycles,
 					context_switches = :context_switches,
-					interrupts = :interrupts, 
-					reboots = :reboots, 
+					interrupts = :interrupts,
+					reboots = :reboots,
 					system_calls = :system_calls,
 					time = :time
 				 WHERE id = :testid AND source_id = :sourceid"
@@ -182,10 +182,10 @@
 			$stmt->bindParam(":system_calls", $performance["system_calls"]);
 			$stmt->bindParam(":time", $performance["time"]);
 			$stmt->execute() or die("Finish(): SQL failed #1");
-			
+
 			if(!$stmt->rowCount())
 				return "Did not update anything!";
-			
+
 			return "OK";
 		}
 	}
