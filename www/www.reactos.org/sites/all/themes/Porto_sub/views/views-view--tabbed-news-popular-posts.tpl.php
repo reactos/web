4482ab6746f4ca659ee85bf18969e3789a35c914
Victor Martinez <vicmarcal@gmail.com>
2016-01-14 22:56:07 +0000

    [VIEWS] - Prepare an unformmated list of rows for News Recent and News Popular posts. - Create a template for the News Recent view in order to create the tabbed bar.
    svn path=/branches/victor-redesign/; revision=925

---
+++ www/www.reactos.org/sites/all/themes/Porto_sub/views/views-view--tabbed-news-popular-posts.tpl.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @file
+ * Main view template.
+ *
+ * Variables available:
+ * - $classes_array: An array of classes determined in
+ *   template_preprocess_views_view(). Default classes are:
+ *     .view
+ *     .view-[css_name]
+ *     .view-id-[view_name]
+ *     .view-display-id-[display_name]
+ *     .view-dom-id-[dom_id]
+ * - $classes: A string version of $classes_array for use in the class attribute
+ * - $css_name: A css-safe version of the view name.
+ * - $css_class: The user-specified classes names, if any
+ * - $header: The view header
+ * - $footer: The view footer
+ * - $rows: The results of the view query, if any
+ * - $empty: The empty text to display if the view is empty
+ * - $pager: The pager next/prev links to display, if any
+ * - $exposed: Exposed widget form/info to display
+ * - $feed_icon: Feed icon to display, if any
+ * - $more: A link to view more, if any
+ *
+ * @ingroup views_templates
+ */
+?>
+<div style="margin-top: 35px;"></div>
+<div class="tabs">
+
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#popularPosts" data-toggle="tab"><i class="icon icon-star"></i> <?php print t('Popular'); ?></a></li>
+		<li><a href="#recentPosts" data-toggle="tab"><?php print t('Recent'); ?></a></li>
+	</ul>
+	
+	<div class="tab-content">
+	
+	  <?php if ($header): ?>
+	    <div class="view-header">
+	      <?php print $header; ?>
+	    </div>
+	  <?php endif; ?>
+	
+	  <?php if ($exposed): ?>
+	    <div class="view-filters">
+	      <?php print $exposed; ?>
+	    </div>
+	  <?php endif; ?>
+	
+	  <?php if ($attachment_before): ?>
+	    <div class="attachment attachment-before">
+	      <?php print $attachment_before; ?>
+	    </div>
+	  <?php endif; ?>
+	
+	  <?php if ($rows): ?>
+    	<div class="tab-pane active" id="popularPosts">
+        <ul class="simple-post-list">
+        <?php print $rows; ?>
+        </ul>
+    	</div>  
+	
+	  <?php elseif ($empty): ?>
+	    <div class="view-empty">
+	      <?php print $empty; ?>
+	    </div>
+	  <?php endif; ?>
+	
+	  <?php if ($pager): ?>
+	    <?php print $pager; ?>
+	  <?php endif; ?>
+	
+	  <?php if ($attachment_after): ?>
+	    <div class="attachment attachment-after">
+	      <?php print $attachment_after; ?>
+	    </div>
+	  <?php endif; ?>
+	
+	  <?php if ($more): ?>
+	    <?php print $more; ?>
+	  <?php endif; ?>
+	
+	  <?php if ($footer): ?>
+	    <div class="tab-pane" id="recentPosts">
+		    <ul class="simple-post-list">
+	        <?php print $footer; ?>
+		    </ul>
+	    </div>  
+	  <?php endif; ?>
+	
+	  <?php if ($feed_icon): ?>
+	    <div class="feed-icon">
+	      <?php print $feed_icon; ?>
+	    </div>
+	  <?php endif; ?>
+	
+  </div>
+</div>
+<?php /* class view */ ?>
---
+++ www/www.reactos.org/sites/all/themes/Porto_sub/views/views-view-unformatted--tabbed-news-recent-posts.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file views-view-unformatted--Porto-view.tpl.php
+ * Porto's custom view unformatted template for all views tagged with "Porto Views".
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<?php foreach ($rows as $id => $row): ?>
+  <?php print $row; ?>
+<?php endforeach; ?>
