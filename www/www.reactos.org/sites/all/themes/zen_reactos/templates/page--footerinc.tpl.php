597c9981dd7212fd7cf83de3c53877314d00a4e9
Victor Martinez <vicmarcal@gmail.com>
2016-01-15 17:24:51 +0000

    [ZEN] Delete the ZEN theme. [ZEN_REACTOS] Delete our own subtheme.
    svn path=/branches/victor-redesign/; revision=927

--- www/www.reactos.org/sites/all/themes/zen_reactos/templates/page--footerinc.tpl.php
+++
@@ -1,9 +0,0 @@
-</div></div><!-- /#main, /#main-wrapper -->
-
-  <div id="rosweb_footer">
-    <p><?php print t("ReactOS is a registered trademark or a trademark of ReactOS Foundation in the United States and other countries."); ?></p>
-  </div><!-- /#rosweb_footer -->
-
-</div></div><!-- /#page, /#page-wrapper -->
-
-<?php print render($page['bottom']); ?>
