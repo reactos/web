84e77058806f9c9241eaf2aad89de6e0a0faa428
Colin Finck <colin@reactos.org>
2016-05-17 01:03:17 +0000

    Add ReCaptcha keys to the private Wiki configuration file.
    svn path=/trunk/; revision=974

--- www/www.reactos.org_config/wiki-config.php
+++ www/www.reactos.org_config/wiki-config.php
@@ -13,3 +13,7 @@ $wgSecretKey = "190071721adb67145f9a02d61f4a84cd2a15980845d562ec64750e83f634c92"
 # Site upgrade key. Must be set to a string (default provided) to turn on the
 # web installer while LocalSettings.php is in place
 $wgUpgradeKey = "132dccc4e3e9ddf";
+
+
+$wgReCaptchaPublicKey = 'your ReCaptcha site key here';
+$wgReCaptchaPrivateKey = 'your ReCaptcha secret key here';
