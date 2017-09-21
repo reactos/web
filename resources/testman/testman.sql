7b768430590c4d4b4334c476986ddd61535e4d0a
Colin Finck <colin@reactos.org>
2017-09-21 10:50:19 +0000

    [TESTMAN] Change the "revision" field to varchar(40) to accommodate for Git hashes and add some keys to accelerate searches. Also make the SQL file consistent with the rest.
    svn path=/trunk/; revision=990

--- resources/testman/testman.sql
+++ resources/testman/testman.sql
@@ -1,17 +1,17 @@
 -- SQL Dump for the "testman" database
 
 CREATE TABLE `sources` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(100) collate latin1_general_ci NOT NULL,
   `password` char(32) collate latin1_general_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 CREATE TABLE `winetest_logs` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `log` mediumblob NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 CREATE TABLE `winetest_results` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -26,14 +26,14 @@ CREATE TABLE `winetest_results` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `test_and_suite` (`test_id`,`suite_id`),
   KEY `suite_id` (`suite_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 CREATE TABLE `winetest_runs` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `finished` tinyint(1) NOT NULL DEFAULT '0',
-  `source_id` bigint(20) unsigned NOT NULL,
-  `revision` int(9) unsigned NOT NULL,
+  `source_id` int(10) unsigned NOT NULL,
+  `revision` varchar(40) NOT NULL,
   `platform` varchar(24) COLLATE latin1_general_ci NOT NULL,
   `comment` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
   `count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Sum of all executed tests',
@@ -44,12 +44,14 @@ CREATE TABLE `winetest_runs` (
   `reboots` int(10) unsigned NOT NULL DEFAULT '0',
   `system_calls` int(10) unsigned NOT NULL DEFAULT '0',
   `time` float unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  PRIMARY KEY (`id`),
+  KEY `revision` (`revision`),
+  KEY `platform` (`platform`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 CREATE TABLE `winetest_suites` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `module` varchar(50) collate latin1_general_ci NOT NULL,
   `test` varchar(50) collate latin1_general_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
