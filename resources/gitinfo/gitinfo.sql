f512b673da1d2561a1655eb11b4614a394f4dd0b
Colin Finck <colin@reactos.org>
2017-10-05 10:33:37 +0000

    [GITINFO] MySQL always creates timestamp columns with a default value and "on update CURRENT_TIMESTAMP". I have to alter the table afterwards to get rid of these unwanted extras again.
    I consider this a bug given that even mysqldump is now unable to identically export and reimport this table..
    At least, the people at Stackoverflow have found this way around it: https://stackoverflow.com/a/31865524
    
    svn path=/trunk/; revision=1007

--- resources/gitinfo/gitinfo.sql
+++ resources/gitinfo/gitinfo.sql
@@ -11,6 +11,13 @@ CREATE TABLE `master_revisions` (
   UNIQUE KEY `rev_hash` (`rev_hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+-- MySQL always creates timestamp columns with a default value and "on update CURRENT_TIMESTAMP".
+-- This is the dirty hack to remove them again (from https://stackoverflow.com/a/31865524).
+-- Even exporting the table structure through mysqldump and importing it again results in these unwanted extras...
+ALTER TABLE `master_revisions` CHANGE COLUMN `commit_timestamp` `commit_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `master_revisions` ALTER COLUMN `commit_timestamp` DROP DEFAULT;
+
+
 CREATE TABLE `master_revisions_todo` (
   `oldrev` char(40) NOT NULL,
   `newrev` char(40) NOT NULL,
