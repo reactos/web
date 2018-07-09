ca4b822ae35a363a78ed3d52dd8c2c7df34e23ff
Colin Finck <colin@reactos.org>
2018-07-09 20:42:07 +0200

    [FORUM] After logging in to RosLogin, redirect to the last forum page visited based on the HTTP_REFERER (because $event['redirect'] is apparently buggy).
    ONLINE-792

--- www/www.reactos.org/forum/ext/reactos/roslogin/event/listener.php
+++ www/www.reactos.org/forum/ext/reactos/roslogin/event/listener.php
@@ -13,10 +13,12 @@
 	class listener implements EventSubscriberInterface
 	{
 		private $_script_path;
+		private $_server_name;
 
 		public function __construct(\phpbb\config\config $config)
 		{
 			$this->_script_path = $config['script_path'];
+			$this->_server_name = $config['server_name'];
 		}
 
 		static public function getSubscribedEvents()
@@ -28,6 +30,25 @@
 
 		public function login_box_before($event)
 		{
-			redirect("/roslogin/?p=login&redirect=" . rawurlencode($this->_script_path . '/' . $event['redirect']));
+			// Due to a bug (?), $event['redirect'] never contains anything else than /forum or /forum/index.php in phpBB 3.2.2.
+			// Therefore, parse the HTTP_REFERER and prefer that over $event['redirect'] if it contains a forum URL.
+			$referer = array_key_exists('HTTP_REFERER', $_SERVER) ? $_SERVER['HTTP_REFERER'] : '';
+			$check_url_1 = 'https://' . $this->_server_name . $this->_script_path . '/';
+			$check_url_2 = 'https://www.' . $this->_server_name . $this->_script_path . '/';
+
+			if (strncmp($referer, $check_url_1, strlen($check_url_1)) === 0)
+			{
+				$redirect = substr($referer, strlen($check_url_1));
+			}
+			else if (strncmp($referer, $check_url_2, strlen($check_url_2)) === 0)
+			{
+				$redirect = substr($referer, strlen($check_url_2));
+			}
+			else
+			{
+				$redirect = $event['redirect'];
+			}
+
+			redirect("/roslogin/?p=login&redirect=" . rawurlencode($this->_script_path . '/' . $redirect));
 		}
 	}
