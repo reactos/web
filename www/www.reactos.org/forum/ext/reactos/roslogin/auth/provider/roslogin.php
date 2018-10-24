2223807233cf9b141f3f0a22b5d6e8dcdaa53db6
Colin Finck <colin@reactos.org>
2018-10-24 02:42:07 +0200

    [FORUM] [ROSLOGIN] Add new users to phpBB's NEWLY_REGISTERED group, which automatically transitions to REGISTERED after X posts.
    We can now set different permissions for newly registered and established users and hopefully combat our spam problems this way.

--- www/www.reactos.org/forum/ext/reactos/roslogin/auth/provider/roslogin.php
+++ www/www.reactos.org/forum/ext/reactos/roslogin/auth/provider/roslogin.php
@@ -47,14 +47,14 @@
 
 		/**
 		 * Generate an array for the user_add() function.
-		 * Taken from the apache.php authentication module and adapted (add an E-Mail address instead of password).
+		 * Taken from the apache.php authentication module and adapted (different group and add an E-Mail address instead of password).
 		 */
 		private function _user_row($username, $email)
 		{
 			// first retrieve default group id
-			$sql = 'SELECT group_id
-				FROM ' . GROUPS_TABLE . "
-				WHERE group_name = '" . $this->db->sql_escape('REGISTERED') . "'
+			$sql = "SELECT group_id
+				FROM " . GROUPS_TABLE . "
+				WHERE group_name = 'NEWLY_REGISTERED'
 				AND group_type = " . GROUP_SPECIAL;
 			$result = $this->db->sql_query($sql);
 			$row = $this->db->sql_fetchrow($result);
