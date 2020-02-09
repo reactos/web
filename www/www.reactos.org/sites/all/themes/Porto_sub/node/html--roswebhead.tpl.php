ddf070e0f562a48183d64af2b296b1e9bbe9d005
Colin Finck <colin@reactos.org>
2020-02-09 18:05:36 +0100

    Remove everything we won't need for the new Hugo-based site.

--- www/www.reactos.org/sites/all/themes/Porto_sub/node/html--roswebhead.tpl.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="<?php print base_path() . drupal_get_path('theme', 'porto'); ?>/vendor/bootstrap/css/bootstrap.css">
-<?php print $styles; ?>
-<?php print $scripts; ?>
-<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
-<?php porto_user_css();?>
